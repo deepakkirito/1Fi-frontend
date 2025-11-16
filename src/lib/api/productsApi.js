869b09131ec1dff36896f52ab689a0dfd78e1425
Deepak Saini <deepakkirito@gmail.com>
2025-11-16 12:32:00 +0530

    Update baseUrl for productsApi to production URL

--- src/lib/api/productsApi.js
+++ src/lib/api/productsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.BACKEND_URL || "http://localhost:4000",
+    baseUrl: process.env.BACKEND_URL || "https://onefi-backend-c8xk.onrender.com",
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
